152c57d22654ffb25c41f92af065e7e078bdebdf
ssa237 <ssa237@cs18272kq.canterbury.ac.nz>
2018-04-17 15:40:33 +1200

    gameEnviornment

--- src/GameEnviornment.java
+++ src/GameEnviornment.java
@@ -1,4 +1,19 @@
+import java.util.Scanner;
 
 public class GameEnviornment {
+	
+	private String teamName = "";
+	
+	
+	public void setGame() {
+		Scanner reader = new Scanner(System.in);
+		while (teamName.length() < 2 || teamName.length() > 10) {
+			System.out.println("What is the name of your super hero team? (Length of Team Name must be 2-10 characters long): ");
+			teamName = reader.next();
+		}
+		
+
+	}
+	
 	
 }
