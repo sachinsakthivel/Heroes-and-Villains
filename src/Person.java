e4afd7a01506905652cb1b053dc239716bb98b06
tkn14 <tkn14@cs18273kq.canterbury.ac.nz>
2018-04-17 15:39:19 +1200

    Created basic person class

--- src/Person.java
+++ src/Person.java
@@ -1,4 +1,62 @@
 
 public class Person {
+	
+	private String personName;
+	private int currentHealth = 100;
+	private int maximumHealth = 100;
+	private boolean living = true;
+	
+	public Person(String name) {
+		personName = name;
+		
+	}
+	
+	public String getPersonName() {
+		return personName;
+	}
+	
+	public void setPersonName(String personName) {
+		this.personName = personName;
+	}
+
+	public int getCurrentHealth() {
+		return currentHealth;
+	}
+
+	public void setCurrentHealth(int currentHealth) {
+		if (maximumHealth > currentHealth) {
+			this.currentHealth = currentHealth; 
+		} else {
+			System.out.println("Maximum health achieved");
+		}
+	}
+
+	public int getMaximumHealth() {
+		return maximumHealth;
+	}
+
+	public void setMaximumHealth(int maximumHealth) {
+		this.maximumHealth = maximumHealth;
+	}
+	
+	public boolean getLiving() {
+		return living;
+	}
+	
+	public void setLiving(boolean living) {
+		this.living = living;
+	}
+	
+	public String toString() {
+		String output = new String("This person is named: " + getPersonName() + " and currently has " + getCurrentHealth() +  " health.");
+		return output; 
+	}
+	
+	
+	
+	
+	
+	
+	
 
 }
