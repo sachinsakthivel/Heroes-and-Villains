ef1514278c2090a712333786d2934ea2195dd613
sachinsakthivel <sachinsakthivel@192.168.1.11>
2018-04-19 19:16:11 +1200

    semi-finished game env

--- src/GameEnviornment.java
+++ src/GameEnviornment.java
@@ -3,16 +3,26 @@ import java.util.Scanner;
 public class GameEnviornment {
 	
 	private String teamName = "";
+	private int teamLength;
+	private int cityNum;
 	
 	
 	public void setGame() {
 		Scanner reader = new Scanner(System.in);
 		while (teamName.length() < 2 || teamName.length() > 10) {
-			System.out.println("What is the name of your super hero team? (Length of Team Name must be 2-10 characters long): ");
+			System.out.println("What is the name of your super hero team?: ");
 			teamName = reader.next();
+			if (teamName.length() < 2 || teamName.length() > 10) {
+				System.out.println("Invalid Input, the Length of Team Name must be 2-10 characters long:");
+			}
 		}
-		while 
-		
+		while (cityNum <= 3 || cityNum >= 6) {
+			System.out.println("How many cities would you like to explore?:");
+			cityNum = reader.nextInt();
+			if (cityNum <= 3 || cityNum >= 6) {
+				System.out.println("Invalid Input, You can choose between 3 to 6 cities");
+			}
+		}
 
 	}
 	
