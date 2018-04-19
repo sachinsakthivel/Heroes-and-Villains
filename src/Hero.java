c032efa729adf8dc5e1f54703846feb1ed1304c1
sachinsakthivel <sachinsakthivel@192.168.1.11>
2018-04-19 20:00:32 +1200

    did more work on hero

--- src/Hero.java
+++ src/Hero.java
@@ -5,6 +5,42 @@ enum Heroability {
 
 public class Hero extends Person{
 	private Heroability ability;
+	private int currentHealth = 100;
+	private int maximumHealth = 100;
+	private boolean living = true;
+	
+	public int getCurrentHealth() {
+		return currentHealth;
+	}
+// not too sure about this function
+	public void setCurrentHealth(int currentHealth) {
+		if (maximumHealth > currentHealth) {
+			this.currentHealth = currentHealth; 
+		} else {
+			System.out.println("Maximum health achieved");
+		}
+	}
+
+	public int getMaximumHealth() {
+		return maximumHealth;
+	}
+
+	public void setMaximumHealth(int maximumHealth) {
+		this.maximumHealth = maximumHealth;
+	}
+	
+	public boolean getLiving() {
+		return living;
+	}
+	
+	public void setLiving(boolean living) {
+		this.living = living;
+	}
+	
+	public String toString() {
+		String output = new String("This person is named: " + getPersonName() + " and currently has " + getCurrentHealth() +  " health.");
+		return output; 
+	}
 
 	public Hero(String name, Heroability type) {
 		super(name);
--- src/Person.java
+++ src/Person.java
@@ -2,8 +2,6 @@
 public class Person {
 	
 	private String personName;
-	private int currentHealth = 100;
-	private int maximumHealth = 100;
 	private boolean living = true;
 	
 	public Person(String name) {
@@ -15,48 +13,5 @@ public class Person {
 		return personName;
 	}
 	
-	public void setPersonName(String personName) {
-		this.personName = personName;
-	}
-
-	public int getCurrentHealth() {
-		return currentHealth;
-	}
-
-	public void setCurrentHealth(int currentHealth) {
-		if (maximumHealth > currentHealth) {
-			this.currentHealth = currentHealth; 
-		} else {
-			System.out.println("Maximum health achieved");
-		}
-	}
-
-	public int getMaximumHealth() {
-		return maximumHealth;
-	}
-
-	public void setMaximumHealth(int maximumHealth) {
-		this.maximumHealth = maximumHealth;
-	}
 	
-	public boolean getLiving() {
-		return living;
-	}
-	
-	public void setLiving(boolean living) {
-		this.living = living;
-	}
-	
-	public String toString() {
-		String output = new String("This person is named: " + getPersonName() + " and currently has " + getCurrentHealth() +  " health.");
-		return output; 
-	}
-	
-	
-	
-	
-	
-	
-	
-
 }
