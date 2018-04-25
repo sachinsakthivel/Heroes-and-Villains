ef0f63cd5a7ccbdd1377acdd752f0e6f2c9e884b
sachinsakthivel <sachinsakthivel@192.168.1.11>
2018-04-25 15:56:07 +1200

    adsf

---
+++ src/Item.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+
+public class Item {
+	private String ItemName;
+	private int ItemPrice;
+	private int ItemDuration;
+	private int ItemStock;
+	
+	public Item(String Name, int Price, int Duration, int Stock) {
+		setItemName(Name);
+		setItemPrice(Price);
+		setItemDuration(Duration);
+		setItemStock(Stock);
+	}
+	
+	public void use(Hero chosenHero) {
+		
+	}
+
+	public String getItemName() {
+		return ItemName;
+	}
+
+	public void setItemName(String itemName) {
+		ItemName = itemName;
+	}
+
+	public int getItemPrice() {
+		return ItemPrice;
+	}
+
+	public void setItemPrice(int itemPrice) {
+		ItemPrice = itemPrice;
+	}
+
+	public int getItemDuration() {
+		return ItemDuration;
+	}
+
+	public void setItemDuration(int itemDuration) {
+		ItemDuration = itemDuration;
+	}
+
+	public int getItemStock() {
+		return ItemStock;
+	}
+
+	public void setItemStock(int itemStock) {
+		ItemStock = itemStock;
+	}
+}
---
+++ src/LesserHeals.java
@@ -0,0 +1,8 @@
+
+public class LesserHeals {
+	
+	public LesserHeals() {
+		LesserHealNum =  rand.nextInt((12 - 7) + 1) + 7;
+		super("LesserHeal", 20, 30, LesserHealNum)
+	}
+}
