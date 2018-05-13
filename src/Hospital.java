e2ff22120917c167fc083f87f65adf1c471c95f0
sachinsakthivel <sachinsakthivel@192.168.1.11>
2018-05-14 00:25:01 +1200

    TIME WORKING, ITEM DURATION FIXED, TIMESTAP NEEDED

--- src/Hospital.java
+++ src/Hospital.java
@@ -1,3 +1,4 @@
+import java.util.*;
 
 public class Hospital extends Locations {
 	
@@ -6,6 +7,8 @@ public class Hospital extends Locations {
 	private Item chosenItem;
 	private int healStartIndex = 0;
 	private int healEndIndex = 3;
+	private ArrayList<Hero> Ward = new ArrayList<Hero>();
+	private Timer timer = new Timer();
 	
 	public Hospital() {
 		super("General Hosptial");
@@ -17,16 +20,17 @@ public class Hospital extends Locations {
 		userInput = -1;
 		while (userInput != 0) {
 			menuOptions();
-			userInput = HelperFunctions.InputValidator(0, 1);
+			userInput = HelperFunctions.InputValidator(0, 2);
 			if (userInput == 1) {
 			chosenHero = HelperFunctions.heroPick(team.getTeam());
 			chosenItem = HelperFunctions.itemPick(team.getInv(), healStartIndex, healEndIndex);
-			System.out.println(chosenHero);
-			System.out.println(chosenItem);
+			healingprocedure(team);
 			System.out.println("You want to Heal Anyone Else?");
+			} else if (userInput == 2) {
+				visitWard();
 			} else {
 				System.out.println("Be Careful Out There, these are Dangerous Times");
-			}
+			} 
 			
 		}		
 	}
@@ -35,6 +39,45 @@ public class Hospital extends Locations {
 		System.out.println("0 - Return To HomeBase");
 		System.out.println("1 - Heal a Hero");
 		System.out.println("2 - Visit the Hero Ward");
+	}
+	
+	public void healingprocedure(Team team) {
+		if (chosenItem.getItemStock() == 0) {
+			System.out.println("Insuffient Required Tickets, Buy from Shop");
+		}else if (chosenHero.getCurrentHealth() == chosenHero.getMaximumHealth()) {
+			System.out.println("Chosen Hero has no need for Healing, Hero is at Full Health.");
+		} else {
+			chosenItem.use(chosenHero);
+			moveToWard(team ,chosenHero);
+			timer.schedule(new TimerTask() {
+				  @Override
+				  public void run() {
+				    releaseFromWard(team, chosenHero);
+				  }
+				}, 2*60*1000);
+		}
+	}
+	
+	public void moveToWard(Team team, Hero hero) {
+		Ward.add(hero);
+		team.removeOffTeam(hero);
+		System.out.println("Hero: "+hero.getPersonName()+ " has been moved to Healing Ward from Team.");
+	}
+	
+	public void releaseFromWard(Team team, Hero hero) {
+		team.addToTeam(hero);
+		Ward.remove(hero);
+		System.out.println("Hero: "+hero.getPersonName()+ " has been released from Healing Ward and is ready to fight again.");
+	}
+	
+	public void visitWard() {
+		if (Ward.size() > 0) {
+			for (Hero patient: Ward) {
+				System.out.println("Person Name: " + patient.getPersonName());
+			}
+		} else {
+			System.out.println("There are currenlty no Heroes Healing in Ward.");
+		}
 	}
 
 }
--- src/lesserHeals.java
+++ src/lesserHeals.java
@@ -5,7 +5,7 @@ public class lesserHeals extends Item {
 	
 	public lesserHeals() {
 		super("Lesser Heal Tickets", 20, 0);
-		ItemDuration = 30;
+		ItemDuration = 1;
 		HealNum = 10;
 	}
 	
@@ -19,8 +19,23 @@ public class lesserHeals extends Item {
 	
 	public String toString() {
 		String returnString = this.getItemName() + " - " + this.getItemStock() + " currently in Team Inventory.";
-		returnString += "\n" + "This item heals " + HealNum + " hp points in " + ItemDuration + " seconds.";
+		returnString += "\n" + "This item heals " + HealNum + " hp points in " + ItemDuration + " minute.";
 		return returnString;
+	}
+	
+	public void use(Hero hero) {
+		if (this.getItemStock() > 0 ) {
+			int heroHealthUpdate = hero.getCurrentHealth() + HealNum;
+			if (heroHealthUpdate < hero.getMaximumHealth()) {
+				hero.setCurrentHealth(heroHealthUpdate);
+			} else {
+				hero.setCurrentHealth(hero.getMaximumHealth());
+			}
+			this.setItemStock(this.getItemStock() - 1);
+			System.out.println("Healing Procedure Has Started");
+		} else {
+			System.out.println("Insufficient amount of "+this.getItemName()+", Please Buy Some from Shop");
+		}
 	}
 	
 }
