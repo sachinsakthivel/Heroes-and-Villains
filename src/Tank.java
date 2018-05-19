05f55593056427c995fac3880ca179de3660e3c8
sachinsakthivel <sachinsakthivel@192.168.1.11>
2018-05-19 15:31:29 +1200

    extended Tank with backstory and getstats function

--- src/Tank.java
+++ src/Tank.java
@@ -1,12 +1,30 @@
 
 public class Tank extends Hero {
 	
+	private static int maxHealth;
+	private static int Armour;
+	private static int Luck;
+	
 	public Tank(String name) {
 		super(name);
-		int maxHealth = 125;
-		int Armour = 5;
+		maxHealth = 125;
+		Armour = 5;
+		Luck = 0;
 		this.setMaximumHealth(maxHealth);
 		this.setCurrentHealth(Armour);
 		this.setArmour(Armour);
+	}
+	
+	public static String background() {
+		String output = "Back story of the Tank is not decided yet.";
+		return output;
+	}
+	
+	public static String getStats() {
+		String output = "Maximum Health: " + maxHealth;
+		output += "\nArmour: " + Armour;
+		output += "\nLuck: " + Luck;
+		return output;
+		
 	}
 }
