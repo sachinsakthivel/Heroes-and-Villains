afba9857f9d19f5af1d0dbed09dacdbcf81a3790
sachinsakthivel <sachinsakthivel@192.168.1.11>
2018-05-20 04:17:23 +1200

    asdf

--- src/StartUpSCREEN.java
+++ src/StartUpSCREEN.java
@@ -69,7 +69,6 @@ public class StartUpSCREEN {
 		JButton btnNewGame = new JButton("NEW GAME");
 		btnNewGame.addActionListener(new ActionListener() {
 			 public void actionPerformed(ActionEvent arg0) {
-				GameEnvironment.launchGameSetupSCREEN();
 				closeSCREEN();
 			}
 		});
