427961c27641721439fcb2a33b346f49680c6850
Sasiru Goonatillake <sgo74@uclive.ac.nz>
2018-05-20 12:02:41 +1200

    Not wired yet

--- src/HomeBaseSCREEN.java
+++ src/HomeBaseSCREEN.java
@@ -8,10 +8,13 @@ import javax.swing.JTextArea;
 import java.awt.Color;
 
 import java.util.concurrent.TimeUnit;
+import javax.swing.JTextField;
+import javax.swing.border.EtchedBorder;
 
 public class HomeBaseSCREEN {
 
 	private JFrame frame;
+	private JTextField txtInputTheNumber;
 
 	/**
 	 * Launch the application.
@@ -47,37 +50,26 @@ public class HomeBaseSCREEN {
 		frame.getContentPane().setLayout(null);
 		
 		
-		JTextArea txtDisplay = new JTextArea("What would you like to do? (Input the Number that Corresponds with your Choice)\n" + 
-				"0 - Quit Game\n1 - Travel North - Destination: Unknown\n" + 
-				"1 - Travel North - Destination: Unknown\n" +
-				"2 - Travel East - Destination: Unknown\n" + 
-				"3 - Travel West - Destination: Unknown\n" + 
-				"4 - Travel South - Destination: Unknown\n" + 
-				"5 - Use a Map\n" + 
-				"6 - Look at Team Status and Item Descriptions");
+		JTextArea txtDisplay = new JTextArea("What would you like to do?\r\n0 - Quit Game\r\n1 - Travel North - Destination: Unknown\r\n1 - Travel North - Destination: Unknown\r\n2 - Travel East - Destination: Unknown\r\n3 - Travel West - Destination: Unknown\r\n4 - Travel South - Destination: Unknown\r\n5 - Use a Map\r\n6 - Look at Team Status and Item Descriptions");
+		txtDisplay.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
 		txtDisplay.setBackground(Color.WHITE);
 		txtDisplay.setWrapStyleWord(true);
 		txtDisplay.setLineWrap(true);
-		txtDisplay.setFont(new Font("Century Gothic", Font.PLAIN, 18));
+		txtDisplay.setFont(new Font("Tw Cen MT", Font.PLAIN, 20));
 		txtDisplay.setEditable(false);
-		txtDisplay.setBounds(160, 320, 682, 319);
+		txtDisplay.setBounds(161, 273, 682, 319);
 		frame.getContentPane().add(txtDisplay);
+		
+		txtInputTheNumber = new JTextField();
+		txtDisplay.setFont(new Font("Tw Cen MT", Font.PLAIN, 20));
+		txtInputTheNumber.setText("Input the Number that Corresponds with your Choice.");
+		txtInputTheNumber.setBounds(267, 603, 460, 30);
+		frame.getContentPane().add(txtInputTheNumber);
+		txtInputTheNumber.setColumns(10);
 		
 		JLabel label = new JLabel("");
 		label.setIcon(new ImageIcon(StartUpSCREEN.class.getResource("/Images/stickBKG.jpg")));
 		label.setBounds(0, 0, 968, 755);
 		frame.getContentPane().add(label);
 	}
-	
-	
-	
-	/*
-	   try {
-			Thread.sleep(3000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	 */
-
 }
